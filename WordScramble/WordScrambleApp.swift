ea158bd5aaa00460c5711c23a8425d3a92b52e82
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-01-19 19:52:55 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ikbal Demirdoven on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
